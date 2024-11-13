63f97b72d664e32f7311efc5afa061f7d94739ac
Mareno <maros.cuninka@gmail.com>
2024-11-13 20:59:23 +0100

    Added foce dynamic for home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,8 @@ import Calendar from "@/components/Calendar"
 import React from "react"
 import { getAllEvents } from "@/app/api/actions"
 
+export const dynamic = "force-dynamic"
+
 const HomePage = async () => {
     // Fetch events from server and pass them as props to the Calendar component
     const events = await getAllEvents()
